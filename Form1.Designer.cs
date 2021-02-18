49f1760410f270f1a5f87a57e838e9ca5c756d1c
DenisRafi <62852838+DenisRafi@users.noreply.github.com>
2021-02-18 17:40:04 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class btnL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnS = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ageStatisticsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database = new WindowsFormsApp1.Database();
+            this.ageStatisticsTableAdapter = new WindowsFormsApp1.DatabaseTableAdapters.AgeStatisticsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ageStatisticsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnS
+            // 
+            this.btnS.Location = new System.Drawing.Point(310, 364);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(75, 23);
+            this.btnS.TabIndex = 0;
+            this.btnS.Text = "&Save";
+            this.btnS.UseVisualStyleBackColor = true;
+            this.btnS.Click += new System.EventHandler(this.btnS_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.ageDataGridViewTextBoxColumn,
+            this.totalDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.ageStatisticsBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 82;
+            this.dataGridView.Size = new System.Drawing.Size(373, 346);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(391, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Range;
+            series1.Legend = "Legend1";
+            series1.Name = "Age";
+            series1.YValuesPerPoint = 6;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(647, 346);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(963, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // ageDataGridViewTextBoxColumn
+            // 
+            this.ageDataGridViewTextBoxColumn.DataPropertyName = "Age";
+            this.ageDataGridViewTextBoxColumn.HeaderText = "Age";
+            this.ageDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            this.ageDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // totalDataGridViewTextBoxColumn
+            // 
+            this.totalDataGridViewTextBoxColumn.DataPropertyName = "Total";
+            this.totalDataGridViewTextBoxColumn.HeaderText = "Total";
+            this.totalDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.totalDataGridViewTextBoxColumn.Name = "totalDataGridViewTextBoxColumn";
+            this.totalDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // ageStatisticsBindingSource
+            // 
+            this.ageStatisticsBindingSource.DataMember = "AgeStatistics";
+            this.ageStatisticsBindingSource.DataSource = this.database;
+            // 
+            // database
+            // 
+            this.database.DataSetName = "Database";
+            this.database.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ageStatisticsTableAdapter
+            // 
+            this.ageStatisticsTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 494);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnS);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "btnL";
+            this.Text = "Link Chart to Database byDR";
+            this.Load += new System.EventHandler(this.btnL_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ageStatisticsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private Database database;
+        private System.Windows.Forms.BindingSource ageStatisticsBindingSource;
+        private DatabaseTableAdapters.AgeStatisticsTableAdapter ageStatisticsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn totalDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class btnL : Form
+    {
+        public btnL()
+        {
+            InitializeComponent();
+        }
+
+        private void btnL_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database.AgeStatistics' table. You can move, or remove it, as needed.
+            this.ageStatisticsTableAdapter.Fill(this.database.AgeStatistics);
+
+        }
+
+        private void btnS_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ageStatisticsBindingSource.EndEdit();
+                ageStatisticsTableAdapter.Update(database.AgeStatistics);
+                MessageBox.Show("Data has been successfully saved.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Age"].XValueMember = "Age";
+            chart1.Series["Age"].YValueMembers = "Total";
+            chart1.DataSource = database.AgeStatistics;
+            chart1.DataBind();
+        }
+    }
+}
